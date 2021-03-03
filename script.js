474cd65f06a9e5223000850a133e8ecd4c5c2771
Sai kishore jalvadi <48144005+Saikishorejalvadi@users.noreply.github.com>
2021-03-02 22:39:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function sub()
+{
+  var a=document.getElementById('num1').value;
+var b=document.getElementById('num2').value;
+  alert(a-b)
+}
+function add()
+{
+  var a=document.getElementById('num1').value;
+var b=document.getElementById('num2').value;
+  alert(a+b)
+}
+function mul()
+{
+  var a=document.getElementById('num1').value;
+var b=document.getElementById('num2').value;
+  alert(a*b)
+}
+function div()
+{
+  var a=document.getElementById('num1').value;
+var b=document.getElementById('num2').value;
+  alert(a/b)
+}
